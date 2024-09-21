71cb0e57532a098f2d360ebf313dda86509dad4f
Farrukh Mansoor <153834913+Farrukhsmit11@users.noreply.github.com>
2024-09-21 11:16:06 +0500

    Update app.js

--- Problem 217/app.js
+++ Problem 217/app.js
@@ -1,7 +1,7 @@
 function containsDuplicate(nums) {
    num.sort((a , b) => a - b ) 
     for (let i = 0; i < nums.length; i++) {
-            if (nums[i] === nums[j]) return true
+            if (nums[i] === nums[i + 1]) return true
         }
     return false
     };
